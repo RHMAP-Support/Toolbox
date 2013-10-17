f2e48e178eac00938ef57dd1d9413f20a10a7113
Brian Dooley <brian.dooley@feedhenry.com>
2013-10-17 11:32:59 +0100

    more code

---
+++ client/default/js/controller/app.js
@@ -0,0 +1,17 @@
+$("#CloudBtn").click(function(){
+	// $("#wrapper").removeClass("hide").addClass("show");
+	$('#wrapper').show();
+	//$("#menuwrapper").removeClass("hide").addClass("show");
+	$("#indexwrapper").addClass("hide");
+	
+});
+
+// $("#web").click(function(){
+// 	$("#wrapper").find("show").removeClass("show").addClass("hide");
+// 	$("#tab2").removeClass("hide").addClass("show");
+// });
+
+// $("#config").click(function(){
+// 	$("#wrapper").find("show").removeClass("show").addClass("hide");
+// 	$("#tab1").removeClass("hide").addClass("show");
+// });
