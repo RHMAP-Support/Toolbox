3deac840e45c5cd4646a449a17f276389d5de6df
Brian Dooley <brian.dooley@feedhenry.com>
2013-10-22 12:32:43 +0100

    everything fine back again

--- cloud/main.js
+++ cloud/main.js
@@ -190,6 +190,6 @@ exports.envs = function(params, callback){
 exports.health = function(params, callback) {
   var html = "<div><p>Everything is Fiiiiiiine.</p></div>";
 
-//  return callback(undefined, html, {'Content-Type' : 'text/html'});
-  return callback(undefined, 'testing 1 2 3 4 5');
+  return callback(undefined, html, {'Content-Type' : 'text/html'});
+//  return callback(undefined, 'testing 1 2 3 4 5');
 };
