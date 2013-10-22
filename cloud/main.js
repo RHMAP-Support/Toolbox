382cc09ea6e500a52a953065d6314b401f5eb735
Brian Dooley <brian.dooley@feedhenry.com>
2013-10-22 12:22:27 +0100

    text only

--- cloud/main.js
+++ cloud/main.js
@@ -190,6 +190,6 @@ exports.envs = function(params, callback){
 exports.health = function(params, callback) {
   var html = "<div><p>Everything is Fiiiiiiine.</p></div>";
 
-  return callback(undefined, html, {'Content-Type' : 'text/html'});
+//  return callback(undefined, html, {'Content-Type' : 'text/html'});
+  return callback(undefined, 'testing');
 };
-
