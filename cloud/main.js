2fe211d3d766de7e9823996f07ead3406d1dbdd8
Brian Dooley <brian.dooley@feedhenry.com>
2013-11-04 12:38:54 +0000

    lazy git

--- cloud/main.js
+++ cloud/main.js
@@ -196,5 +196,5 @@ exports.health = function(params, callback) {
   var html = "<div><p>Everything is Fiiiiiiine.</p></div>";
 
 //  return callback(undefined, html, {'Content-Type' : 'text/html'});
-  return callback(undefined, 'Everything is reeeeeaaal goooooood');
+  return callback(undefined, 'Everything is good');
 };
